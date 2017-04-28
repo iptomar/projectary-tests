6c4e6b62f3924ecb761f9714a8007b1571d1933d
Bruno Freitas <l0custdev@gmail.com>
2017-04-28 17:05:12 +0100

    Change to functions for more modular aproach to testing

--- API/main.js
+++ API/main.js
@@ -1,37 +1,69 @@
 var request = require('supertest');
 var url = "http://localhost:8080";
 
-// POST ------------------------------------------------------------------------
-describe("POST", function (){
-  it("/user", function(done){
-    var user = {
-  	"name": "Teste",
-  	"external":  "1",
-  	"type": "1",
-  	"email": "teste@ipt.pt",
-  	"phone": "911234567",
-  	"password": "secret"
-  };
-  request(url)
-    .post("/user")
-    .send(user)
-    .expect(200, done)
-});
-
-// GET -------------------------------------------------------------------------
-describe("GET", function (){
-  it("/user", function(done){
+// USERS -----------------------------------------------------------------------
+/* user format -----------------------------------------------------------------
+	"name": "Teste",
+	"external":  "1",
+	"type": "1",
+	"email": "teste@ipt.pt",
+	"phone": "911234567",
+	"password": "secret"
+};*/
+function postUser(user){
+  describe("POST User", function (){
+    it("Create user", function(done){
     request(url)
-    .get("/user")
-    .auth("teste@ipt.pt","secret") // Authentication
-    .expect(200, done)
-  });
-
-  it("/user/:id", function(done){
-    request(url)
-    .get("/user/1")
-    .auth("teste@ipt.pt","secret")
-    .expect(200, done)
+      .post("/user")
+      .send(user)
+      .expect(200, done)
     });
   });
-});
+};
+
+function getUser(index){
+  var searchString = "/user";
+  if (index != null) searchString = "/user/" + index;
+  describe("GET User", function (){
+    it("Return users", function(done){
+      request(url)
+      .get(searchString)
+      .auth("teste@ipt.pt","secret")
+      .expect(200, done)
+    });
+  });
+};
+
+// PROJECTS --------------------------------------------------------------------
+/* project format --------------------------------------------------------------
+  "name": "Teste",
+  "description":  "Something",
+  "course": "1"
+*/
+function postProject(project){
+  describe("POST Project", function (){
+    it("Create Project", function(done){
+      request(url)
+        .post("/project")
+        .auth("teste@ipt.pt", "secret")
+        .send(project)
+        .expect(200, done)
+    });
+  });
+};
+
+function getProject(index){
+  var searchString = "/project";
+  if (index != null) searchString = "/project/" + index;
+  describe("GET", function (){
+    it("Return projects", function(done){
+      request(url)
+      .get(searchString)
+      .auth("teste@ipt.pt","secret")
+      .expect(200, done)
+    });
+  });
+};
+
+// TESTS -----------------------------------------------------------------------
+getProject(1);
