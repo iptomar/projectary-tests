b9c976d99c4032ce96a0f7985e7e713307492314
Bruno Freitas <l0custdev@gmail.com>
2017-04-28 12:25:52 +0100

    Implements GET testing with authentication

--- API/main.js
+++ API/main.js
@@ -1,19 +1,37 @@
 var request = require('supertest');
 var url = "http://localhost:8080";
 
+// POST ------------------------------------------------------------------------
 describe("POST", function (){
-  it("POST user", function(done){
+  it("/user", function(done){
     var user = {
-  	"name": "aluno",
+  	"name": "Teste",
   	"external":  "1",
   	"type": "1",
-  	"email": "email1",
+  	"email": "teste@ipt.pt",
   	"phone": "911234567",
   	"password": "secret"
   };
+  request(url)
+    .post("/user")
+    .send(user)
+    .expect(200, done)
+});
+
+// GET -------------------------------------------------------------------------
+describe("GET", function (){
+  it("/user", function(done){
     request(url)
-      .post("/user")
-      .send(user)
-      .expect(200, done)
+    .get("/user")
+    .auth("teste@ipt.pt","secret") // Authentication
+    .expect(200, done)
+  });
+
+  it("/user/:id", function(done){
+    request(url)
+    .get("/user/1")
+    .auth("teste@ipt.pt","secret")
+    .expect(200, done)
+    });
   });
 });
