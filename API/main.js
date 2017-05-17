8408e6be3f526139614a262009deb26ba02fbec0
Bruno Freitas <l0custdev@gmail.com>
2017-05-17 16:56:55 +0100

    Add authentication test
    Add teacher creation test
    Add school listing test
    Add course listing test

--- API/main.js
+++ API/main.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var url = "http://localhost:8080";
 
 // USERS -----------------------------------------------------------------------
-/* user format -----------------------------------------------------------------
+/*
 	"name": "Teste",
 	"external":  "1",
 	"type": "1",
@@ -10,8 +10,21 @@ var url = "http://localhost:8080";
 	"phone": "911234567",
 	"password": "secret"
 };*/
+
+function authUser(){
+  describe("LOGIN", function(){
+    it("Auth", function(done){
+    request(url)
+      .post("/login")
+      .auth("teste@ipt.pt","secret")
+      .send({"username":"teste@ipt.pt", "password":"secret"})
+      .expect(200, done)
+    });
+  });
+};
+
 function postUser(user){
-  describe("POST User", function (){
+  describe("POST", function (){
     it("Create user", function(done){
     request(url)
       .post("/user")
@@ -24,7 +37,7 @@ function postUser(user){
 function getUser(index){
   var searchString = "/user";
   if (index != null) searchString = "/user/" + index;
-  describe("GET User", function (){
+  describe("GET", function (){
     // Get user/users info
     it("Return users", function(done){
       request(url)
@@ -34,20 +47,38 @@ function getUser(index){
       .end(function(err, res){
         if (err) return done(err);
         done();
-        console.log(res.body);
       });
     });
   });
 };
 
+/* TEACHERS --------------------------------------------------------------
+/*
+  "name": "user",
+  "external_id": "15000",
+  "email": "user@ipt.pt",
+  "phonenumber": "555123123",
+  "password": "secret"
+*/
+function postTeacher(teacher){
+  describe("POST", function(){
+    it("Create teacher", function(done){
+    request(url)
+      .post("/user")
+      .send(teacher)
+      .expect(200, done)
+    });
+  });
+}
+
 // PROJECTS --------------------------------------------------------------------
-/* project format --------------------------------------------------------------
+/*
   "name": "Teste",
   "description":  "Something",
   "course": "1"
 */
 function postProject(project){
-  describe("POST Project", function (){
+  describe("POST", function (){
     it("Create Project", function(done){
       request(url)
         .post("/project")
@@ -70,6 +101,40 @@ function getProject(index){
       .end(function(err, res){
         if (err) return done(err);
         done();
+      });
+    });
+  });
+};
+
+// SCHOOLS --------------------------------------------------------------------
+function getSchools(){
+  describe("GET", function (){
+    it("Return schools", function(done){
+      request(url)
+        .get("/school")
+        .auth("teste@ipt.pt", "secret")
+        .expect(200)
+        .end(function(err, res){
+          if(err) return done(err);
+          done();
+        });
+    });
+  });
+};
+
+// COURSES --------------------------------------------------------------------
+function getCourse(index){
+  var searchString = "/course";
+  if (index != null) searchString = "/course/" + index;
+  describe("GET", function (){
+    it("Return courses", function(done){
+      request(url)
+      .get(searchString)
+      .auth("teste@ipt.pt","secret")
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        done();
         console.log(res.body);
       });
     });
@@ -77,14 +142,3 @@ function getProject(index){
 };
 
 // TESTS -----------------------------------------------------------------------
-/*var user = {
-  "name": "Teste",
-	"external":  "1",
-	"type": "1",
-	"email": "teste@ipt.pt",
-	"phone": "911234567",
-	"password": "secret"
-}
-postUser(user);*/
-
-getUser();
