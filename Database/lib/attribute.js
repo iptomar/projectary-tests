11d2522b11973e4edb7febe7b951b47c4f353f57
Freakmania <henribernardo@hotmail.com>
2017-05-31 00:19:30 +0100

    Add various database table tests

---
+++ Database/lib/attribute.js
@@ -0,0 +1,63 @@
+var utils = new (require('./utils.js'))();
+
+class Attribute {
+
+  /**
+   * Truncate the attribute table and test insertions
+   */
+  async start(connection) {
+    this.connection = connection;
+
+    try {
+      await this.truncate();
+      await this.insertAttributes();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  /**
+   * Truncate the attribute table so we while performing insertions
+   * we can correctly track the number of rows before and 
+   * after the insertions.
+   */
+  async truncate() {
+    await utils.cmd(`
+      mysql --defaults-file="./.my.cnf" -e "TRUNCATE projectary_tests.attribute;"
+      `, 'Truncated table atribute', 'Failed to truncate table attribute');
+  }
+
+  /**
+   * Insert 5 attributes and check if they're inserted by counting
+   * the number of rows before and after the insertion of attributes.
+   */
+  async insertAttributes() {
+    try {
+      var rowsCount;
+
+      await this.connection.query('SELECT * FROM projectary_tests.attribute;', await function (error, results, fields) {
+        rowsCount = results.length;
+      });
+
+      // mysqltest
+      try {
+        await utils.execPromise(`mysqltest --defaults-file="./.my.cnf" --database projectary_tests < sql/insertAttributes.sql`);
+      } catch (error) {
+        throw new Error(error);
+      }
+
+      await this.connection.query('SELECT * FROM projectary_tests.attribute;', await function (error, results, fields) {
+        if (rowsCount + 5 == results.length) {
+          utils.log('success', 'Inserted 5 attributes successfully');
+        } else {
+          utils.log('fail', 'The number of rows before and after the insertion do not match');
+        }
+      });
+    } catch (error) {
+      utils.log('fail', 'Failed to insert attributes \n' + error);
+      return;
+    }
+  }
+}
+
+module.exports = Attribute;
---
+++ Database/lib/groupuser.js
@@ -0,0 +1,63 @@
+var utils = new (require('./utils.js'))();
+
+class Groupuser {
+
+  /**
+   * Truncate the groupuser table and test insertions
+   */
+  async start(connection) {
+    this.connection = connection;
+
+    try {
+      await this.truncate();
+      await this.insertGroupusers();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  /**
+   * Truncate the groupuser table so we while performing insertions
+   * we can correctly track the number of rows before and 
+   * after the insertions.
+   */
+  async truncate() {
+    await utils.cmd(`
+      mysql --defaults-file="./.my.cnf" -e "TRUNCATE projectary_tests.groupuser;"
+      `, 'Truncated table groupuser', 'Failed to truncate table groupuser');
+  }
+
+  /**
+   * Insert 5 groupusers and check if they're inserted by counting
+   * the number of rows before and after the insertion of groupusers.
+   */
+  async insertGroupusers() {
+    try {
+      var rowsCount;
+
+      await this.connection.query('SELECT * FROM projectary_tests.groupuser;', await function (error, results, fields) {
+        rowsCount = results.length;
+      });
+
+      // mysqltest
+      try {
+        await utils.execPromise(`mysqltest --defaults-file="./.my.cnf" --database projectary_tests < sql/insertgroupusers.sql`);
+      } catch (error) {
+        throw new Error(error);
+      }
+
+      await this.connection.query('SELECT * FROM projectary_tests.groupuser;', await function (error, results, fields) {
+        if (rowsCount + 5 == results.length) {
+          utils.log('success', 'Inserted 5 groupusers successfully');
+        } else {
+          utils.log('fail', 'The number of rows before and after the insertion do not match');
+        }
+      });
+    } catch (error) {
+      utils.log('fail', 'Failed to insert groupusers \n' + error);
+      return;
+    }
+  }
+}
+
+module.exports = Groupuser;
