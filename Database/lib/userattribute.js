6ea6c88704f5fa52d531db0b8cbc774e11cd28e9
Freakmania <henribernardo@hotmail.com>
2017-05-31 01:07:27 +0100

    Add and update database table tests, also update Karma config

---
+++ Database/lib/userattribute.js
@@ -0,0 +1,63 @@
+var utils = new (require('./utils.js'))();
+
+class Userattribute {
+
+  /**
+   * Truncate the userattribute table and test insertions
+   */
+  async start(connection) {
+    this.connection = connection;
+
+    try {
+      await this.truncate();
+      await this.insertUserattributes();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  /**
+   * Truncate the userattribute table so we while performing insertions
+   * we can correctly track the number of rows before and 
+   * after the insertions.
+   */
+  async truncate() {
+    await utils.cmd(`
+      mysql --defaults-file="./.my.cnf" -e "TRUNCATE projectary_tests.userattribute;"
+      `, 'Truncated table userattribute', 'Failed to truncate table userattribute');
+  }
+
+  /**
+   * Insert 5 userattributes and check if they're inserted by counting
+   * the number of rows before and after the insertion of userattributes.
+   */
+  async insertUserattributes() {
+    try {
+      var rowsCount;
+
+      await this.connection.query('SELECT * FROM projectary_tests.userattribute;', await function (error, results, fields) {
+        rowsCount = results.length;
+      });
+
+      // mysqltest
+      try {
+        await utils.execPromise(`mysqltest --defaults-file="./.my.cnf" --database projectary_tests < sql/insertUserattributes.sql`);
+      } catch (error) {
+        throw new Error(error);
+      }
+
+      await this.connection.query('SELECT * FROM projectary_tests.userattribute;', await function (error, results, fields) {
+        if (rowsCount + 5 == results.length) {
+          utils.log('success', 'Inserted 5 userattributes successfully');
+        } else {
+          utils.log('fail', 'The number of rows before and after the insertion do not match');
+        }
+      });
+    } catch (error) {
+      utils.log('fail', 'Failed to insert userattributes \n' + error);
+      return;
+    }
+  }
+}
+
+module.exports = Userattribute;
