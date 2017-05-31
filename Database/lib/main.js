bf57614728a748f724cb2dd6906a7b06279d8b9c
Freakmania <henribernardo@hotmail.com>
2017-05-31 01:21:23 +0100

    Update main.js

--- Database/lib/main.js
+++ Database/lib/main.js
@@ -11,6 +11,12 @@ var course = new (require('./course.js'))();
 var group = new (require('./group.js'))();
 var project = new (require('./project.js'))();
 var application = new (require('./application.js'))();
+var attribute = new (require('./attribute.js'))();
+var courseyear = new (require('./courseyear.js'))();
+var groupuser = new (require('./groupuser.js'))();
+var projectattribute = new (require('./projectattribute.js'))();
+var projectteacher = new (require('./projectteacher.js'))();
+var userattribute = new (require('./userattribute.js'))();
 
 // Loads MySQL config file
 var config = ini.parse(fs.readFileSync('./.my.cnf', 'utf8'));
@@ -44,7 +50,6 @@ async function start() {
     await project.start(connection);
     // start application table tests
     await application.start(connection);
-    
      // start attribute table tests
     await attribute.start(connection);
     // start courseyear table tests
@@ -54,7 +59,7 @@ async function start() {
     // start projectattribute table tests
     await projectattribute.start(connection);
     // start projectteacher  table tests
-    await projectteacher .start(connection);
+    await projectteacher.start(connection);
     // start userattribute table tests
     await userattribute.start(connection);
 
