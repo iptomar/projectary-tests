0a83a414567fd42d5fbdf01ee1d2399cb9ce8ed5
asantos3 <andrewskad@gmail.com>
2017-06-07 10:48:50 +0100

    Update DB TODO and insertGroupUsers.sql

--- Database/sql/tables/insertGroupUsers.sql
+++ Database/sql/tables/insertGroupUsers.sql
@@ -4,8 +4,8 @@ SET NAMES 'utf8';
 
 # groupid, userid, owner, grade, approvedin
 # Insert 5 groupuser
-INSERT INTO `groupuser` VALUES (1, 1, 1, 10.0, '2017-01-04 00:00:01');
-INSERT INTO `groupuser` VALUES (2, 2, 1, 10.0, '2017-01-04 00:00:01');
-INSERT INTO `groupuser` VALUES (3, 3, 1, 10.0, '2017-01-04 00:00:01');
-INSERT INTO `groupuser` VALUES (4, 4, 1, 10.0, '2017-01-04 00:00:01');
-INSERT INTO `groupuser` VALUES (5, 5, 1, 10.0, '2017-01-04 00:00:01');
+INSERT INTO `groupuser` VALUES (1, 1, 1, NULL, '2017-01-04 00:00:01');
+INSERT INTO `groupuser` VALUES (2, 2, 1, NULL, '2017-01-04 00:00:01');
+INSERT INTO `groupuser` VALUES (3, 3, 1, NULL, '2017-01-04 00:00:01');
+INSERT INTO `groupuser` VALUES (4, 4, 1, NULL, '2017-01-04 00:00:01');
+INSERT INTO `groupuser` VALUES (5, 5, 1, NULL, '2017-01-04 00:00:01');
