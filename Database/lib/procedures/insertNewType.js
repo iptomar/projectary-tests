d909285985445a99a341e6678b0610872ddb80c7
Freakmania <henribernardo@hotmail.com>
2017-06-09 00:59:59 +0100

    Done insertNewTipe procedure and update insertion on project table
    project table was modified by the database team, the insertion is now updated

--- Database/lib/procedures/insertNewType.js
+++ Database/lib/procedures/insertNewType.js
@@ -1,1 +1,59 @@
 var utils = new (require('./../utils.js'))();
+
+class InsertNewType {
+
+  /**
+   * Truncate the user table and test the insertNewType procedure
+   */
+  async start(connection) {
+    this.connection = connection;
+
+    try {
+      await this.truncate();
+      await this.testInsertNewType();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  /**
+   * Truncate the type table so we can test the procedure
+   * without having other interferences
+   */
+  async truncate() {
+    await utils.cmd(`
+      mysql --defaults-file="./.my.cnf" -e "TRUNCATE projectary_tests.type;"
+      `, 'Truncated table type', 'Failed to truncate table type');
+  }
+
+  /**
+   * 
+   */
+  async testInsertNewType() {
+    try {
+      var typeStatus;
+
+      // calls procedure insertNewType and inserts a new courseyear
+      await this.connection.query("CALL projectary_tests.insertNewType('descricao');"); 
+
+      //
+      await this.connection.query("(SELECT EXISTS(SELECT `desc` FROM type WHERE `desc` LIKE 'descricao') as output)", 
+      
+      await function (error, results, fields) {
+        //console.log(results[0]['output']);
+        typeStatus=results[0]['output'];
+        if (typeStatus == 1 ) {
+          utils.log('success', 'insertNewType procedure called successfully');
+        } else {
+          utils.log('fail', 'Failed to call the insertNewType procedure');
+        }
+      });
+
+    } catch (error) {
+      utils.log('fail', 'Failed to test the insertNewType procedure \n' + error);
+      return;
+    }
+  }
+}
+
+module.exports = InsertNewType;
