6d588e09eaf44ecbe59643130e394d662d86a919
migsilva95 <miguelflsilva@hotmail.com>
2017-06-09 15:06:20 +0100

    Corrected error because BD changing

--- Database/sql/procedures/insertProject.sql
+++ Database/sql/procedures/insertProject.sql
@@ -3,5 +3,5 @@ SET SQL_WARNINGS=1;
 SET NAMES 'utf8';
 
 # id, approvedin, year, courseid, name, descreption, userid, created
-INSERT INTO `project` VALUES (1, null, 2017, 1, 'teste','teste',1,null);
-INSERT INTO `project` VALUES (2, NOW(), 2017, 2, 'teste','teste',2,null);
+INSERT INTO `project` VALUES (1, null, 2017, 1, 'teste','teste',1,null,null,'0');
+INSERT INTO `project` VALUES (2, NOW(), 2017, 2, 'teste','teste',2,null,null,'0');
