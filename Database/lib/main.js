0ac23bed30991381d78faa98d249aab8b59bf395
Rui Rodrigues <rman2006@hotmail.com>
2017-09-26 02:51:33 +0100

    Added costumizable delay between scripts and corresponding CLI opttion. Added available procedures tests listing

--- Database/lib/main.js
+++ Database/lib/main.js
@@ -6,15 +6,22 @@ var parseArgs = require('minimist');
 const params = parseArgs(process.argv);
 //default batch size
 var batch = 2;
+//time in ms between tests to make sure mysql settles after each one
+var delay=0;
+//recomended ratio between batch size and delay to assure reduced load on mysql
+const ratio=5;
 var options="";
 var scripts="";
 //tables scripts array
 var tablesScripts = [];
+//procedures scripts array
+var procsScripts = [];
 //log file
 var logfile = "results.log"
 // Loads modules
 var database = new (require('./database.js'))();
-/*var activeUser = new (require('./procedures/activateUser.js'))();
+/*
+var activeUser = new (require('./procedures/activateUser.js'))();
 //var addToGroup = new (require('./procedures/addToGroup.js'))();
 //var deleteGroup = new (require('./procedures/deleteGroup.js'))();
 //var deleteUserAttribute = new (require('./procedures/deleteUserAttribute.js'))();
@@ -58,17 +65,26 @@ var connection = mysql.createConnection({
   multipleStatements: true // Run queries with multiple statements
 });
 
-//tests all or designated table
-async function tableScript(tablesScripts, script) {
+//tests all or designated script
+async function scriptTest(scriptArray, script) {
 	var tested = 0;
 	if(isNaN(script) || script==null)
-	for (var i=0;i<tablesScripts.length;i++) {
-		var name = tablesScripts[i].split('/').pop();
+	for (var i=0;i<scriptArray.length;i++) {
+		var name = scriptArray[i].split('/').pop();
 		if (script==null || (script!=null && script.toUpperCase()==name.split('/').pop().substring(0,name.indexOf('.')).toUpperCase())){
 			var test = new (require("./tables/"+name))();
-			await test.start(connection, logfile, batch);
-			tested++;
-				 // await utils.timer(1,10000,"Delaying until next test...");
+			await test.start(connection, logfile, batch).then(	
+			function() { 
+				utils.log('warning', "Waiting...\r\n");
+				//Forces delay between tests to assure low mysql server load after previous test
+				return new Promise(async function(resolve, reject) {
+				await setTimeout(function() {
+				utils.log('success', "Resuming...");
+				  resolve();
+				}, delay);
+			  });			
+			} );
+			await tested++;
 		}
 	}
 	//checks if there was a valid test
@@ -76,6 +92,7 @@ async function tableScript(tablesScripts, script) {
 	console.log("\r\n");
 }
 
+	  
 async function CLI(params){
 	//console.log(params);
 	//CLI option -lt -tl Lists tables teste scripts
@@ -87,18 +104,31 @@ async function CLI(params){
 		console.log("\r\n");
 		}
 	//CLI option -lp -pl Lists procedures test scripts
-	else if (params.l && params.p) console.log("output procedure scripts");
-	//CLI option -t tests specific script. Accpets script name and/or batch size
+	else if (params.l && params.p) { 
+		console.log("\r\nAvailable procedures scripts:\r\n");
+		for (var i=0; i<procsScripts.length;i++){ 
+			var name = procsScripts[i].split('/').pop();
+			console.log(name.split('/').pop().substring(0,name.indexOf('.')).toUpperCase());  }
+		console.log("\r\n");
+		}
+	//CLI option -t tests specific table script. Accepts script name and/or batch size
 	else if (params.t!=null && params.t!=0 && params.t!=1 && !params.l) {
 		if (parseInt(params._[2])>2) batch = parseInt(params._[2]); 
 		await initDB();
-		await tableScript(tablesScripts, params.t); }
-	else if (params.p && params.l!=null && params.l!=0 && params.l!=1) console.log("test specific procedure"); 
-	//CLI option -T tests all tables scripts. Accepts batch size
+		await scritTest(tablesScripts, params.t); }
+	//CLI option -p tests specific procedure script. Accepts script name and/or batch size
+	else if (params.p && params.l!=null && params.l!=0 && params.l!=1)  {
+		if (parseInt(params._[2])>2) batch = parseInt(params._[2]); 
+		await initDB();
+		await procScript(procsScripts, params.t); }
+	//CLI option -T tests all tables scripts. Accepts batch size and delay -d
 	else if (params.T || !isNaN(params.T)) {
 		if (parseInt(params.T)>2) batch = parseInt(params.T);
 		await initDB();		
-		await tableScript(tablesScripts, null); 
+		//if not set by user sets delay for a safe value
+		if (params.d>0? delay=params.d : delay=batch/ratio);
+		console.log("\r\nUsing a " + delay + " ms delay between tests.\r\n");
+		await scriptTest(tablesScripts, null); 
 	}
 	//CLI option -P tests all procedures scripts. Accepts batch size
 	else if (params.P) console.log ("test all procedures");
@@ -112,7 +142,6 @@ async function CLI(params){
 }
 
 async function initDB(){
-
     //starts database tests and their tables
     await console.log("\nGrabbing the database dump and test the integrity...");
     await database.start();
@@ -124,24 +153,14 @@ async function initDB(){
 async function start() {
   try {
   	await utils.getScripts("./lib/tables/",".js",tablesScripts);
+  	await utils.getScripts("./lib/procedures/",".js",procsScripts);
 	await CLI(params);
-	//dev area
-    /**
-     * Testing the database procedures
-     */
-	/*var proceduresScripts = [];
-	await utils.getScripts("./lib/procedures/",".js",proceduresScripts);
-	for (var i=0;i<proceduresScripts.length; i++) {
-		var test = new (require("./procedures/"+proceduresScripts[i].split('/').pop()))();	
-		await test.start(connection);
-	*/
-	//end of dev area
 	await connection.end();
 	console.log("\r\nTask finished.\r\n")
    } catch (error) {
-    await connection.end();
-    return;
-  }
+		await connection.end();
+     return;
+   }
 }
 
 module.exports.start = start;
